f9c7d6df3be81b4960960b5ee5facebecc3ce7ee
Felix Ståhl <31844847+felixstahl@users.noreply.github.com>
2024-02-25 10:12:50 +0100

    First commit

---
+++ src/main/java/com/testing/springgen/SpringGenAppApplication.java
@@ -0,0 +1,13 @@
+package com.testing.springgen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGenAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGenAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testing/springgen/SpringGenAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testing.springgen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGenAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
